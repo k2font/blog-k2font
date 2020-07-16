7523b77abb5b57eecf23b441f392c26838895f7e
k2font <shoichiro.kouno@gmail.com>
2020-07-16 15:40:53 +0900

    一部のコンポーネントをTypeScript化した

---
+++ src/pages/blog.tsx
@@ -0,0 +1,76 @@
+import React from "react"
+import { Link, graphql, PageProps } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+import Button from "../components/button"
+
+export default function Blog(props: PageProps) {
+  const { data } = props
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMdx.edges
+
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO title="All posts" />
+      <Bio />
+      <div style={{ margin: "20px 0 40px" }}>
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <div key={node.fields.slug}>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link
+                  style={{ boxShadow: `none` }}
+                  to={`/blog${node.fields.slug}`}
+                >
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </div>
+          )
+        })}
+      </div>
+      <Link to="/">
+        <Button marginTop="85px">Go Home</Button>
+      </Link>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
